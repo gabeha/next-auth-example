531512b8dd1ccefdfb144cc0fbf7dccf6edc2997
gabeha <gabriel.hauss@gmail.com>
2024-09-20 10:19:35 +0200

    removed the redirect roxy url

--- auth.ts
+++ auth.ts
@@ -21,7 +21,6 @@ const config = {
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
-      redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
     }),
   ],
   basePath: "/api/auth",
